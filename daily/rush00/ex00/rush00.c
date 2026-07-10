86569deae30df23d7de907510efcf5968a21587e
anon <anon@example.com>
2026-07-10 19:55:53 +0100

    rush00.c

--- daily/rush00/ex00/rush00.c
+++ daily/rush00/ex00/rush00.c
@@ -11,36 +11,55 @@
 /* ************************************************************************** */
 void	ft_putchar(char c);
 
-void	print_row(int x, char left, char fill, char right)
+/*
+** Prints one line of the rectangle.
+** is_top_or_bottom tells us whether this is an "o---o" line
+** or a "|   |" line.
+*/
+void	print_one_line(int width, int is_top_or_bottom)
 {
-	int	i;
+	int	column;
 
-	ft_putchar(left);
-	i = 1;
-	while (i < x - 1)
+	column = 0;
+	while (column < width)
 	{
-		ft_putchar(fill);
-		++i;
+		if (column == 0 || column == width - 1)
+		{
+			/* first or last character: always a corner */
+			ft_putchar('o');
+		}
+		else if (is_top_or_bottom == 1)
+		{
+			/* middle of a top/bottom row: dash */
+			ft_putchar('-');
+		}
+		else
+		{
+			/* middle of a normal row: space */
+			ft_putchar(' ');
+		}
+		column = column + 1;
 	}
-	if (x > 1)
-		ft_putchar(right);
 	ft_putchar('\n');
 }
 
-void	rush(int x, int y)
+/*
+** Draws a rectangle: width characters wide, height lines tall.
+*/
+void	rush(int width, int height)
 {
-	int	j;
+	int	row;
 
-	if (x <= 0 || y <= 0)
+	if (width <= 0 || height <= 0)
 		return ;
-	j = 0;
-	while (j < y)
+	row = 0;
+	while (row < height)
 	{
-		if (j == 0 || j == y - 1)
-			print_row(x, 'o', '-', 'o');
+		if (row == 0 || row == height - 1)
+			print_one_line(width, 1);
 		else
-			print_row(x, '|', ' ', '|');
-		++j;
+			print_one_line(width, 0);
+		row = row + 1;
 	}
 }
 /* vim: set noet ts=4 sw=4 tw=80 : */
