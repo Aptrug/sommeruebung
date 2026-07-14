2360f691d1655fac6a629eec31ebf1b287563ec8
anon <anon@example.com>
2026-07-14 05:32:50 +0100

    solve.sh

--- daily/shell01/ex08/solve.sh
+++ daily/shell01/ex08/solve.sh
@@ -3,7 +3,7 @@ set -eu
 install -m 755 /dev/stdin add_chelou.sh << 'EOF'
 #!/bin/sh
 echo "$FT_NBR1 + $FT_NBR2" \
-	| tr "\\\\'\"\?\!mrdoc" "0123401234" \
+	| tr "\\'\"?!mrdoc" "0123401234" \
 	| xargs echo "obase=13;ibase=5;" \
 	| bc \
 	| tr "0123456789ABC" "gtaio luSnemf"
