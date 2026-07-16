82ddfd71db80f846fe5edcc9b94b8a899cd5a134
anon <anon@example.com>
2026-07-17 00:20:03 +0100

    rev_print.c

--- old/exams/level1/07_rev_print_medium/rev_print.c
+++ old/exams/level1/07_rev_print_medium/rev_print.c
@@ -1,34 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                                            */
-/*   rev_print.c                                                              */
-/*                                                                            */
-/*   By: ysabraou <ysabraou@student.1337.ma>                                  */
-/*                                                                            */
-/*   Created: 2026/07/11 14:22:33 by ysabraou                                 */
-/*   Updated: 2026/07/11 14:22:33 by ysabraou                                 */
+/*                                                        :::      ::::::::   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysabraou <ysabraou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/17 00:18:21 by ysabraou          #+#    #+#             */
+/*   Updated: 2026/07/17 00:18:21 by ysabraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 int	main(int argc, char **argv)
 {
+	int		len;
 	int		i;
+	char	tmp;
 	char	*str;
 
-	if (argc != 2)
+	if (argc == 2)
 	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	i = 0;
-	str = argv[1];
-	while (str[i])
-		++i;
-	while (i > 0)
-	{
-		--i;
-		write(1, &str[i], 1);
+		str = argv[1];
+		len = 0;
+		while (str[len])
+			len++;
+		i = 0;
+		while (i < len / 2)
+		{
+			tmp = str[i];
+			str[i] = str[len - 1 - i];
+			str[len - 1 - i] = tmp;
+			i++;
+		}
+		write(1, str, len);
 	}
 	write(1, "\n", 1);
 	return (0);
